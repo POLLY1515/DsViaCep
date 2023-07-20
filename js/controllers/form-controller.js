a4719f46c77f6cf338156738e987bdb51240df14
POLLY1515 <amarantepolly2030@gmail.com>
2023-07-20 01:25:55 -0300

    Iniciando o sistema javascript

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,6 @@
+
+//Sera responsavel por iniciar o funcionamento do controlador
+export function init(){
+    const inputCep = document.forms.newAddress.cep//pegando o campo pelo forms
+    console.log(inputCep)
+}
